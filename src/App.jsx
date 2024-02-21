8007faf84ce491a76c1e173154250437bbac7747
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-02-21 12:32:30 -0500

    All component structures are built. I not need to address content and functionallity

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './components/sidebar';
-import  PrimaryDash  from './components/primary-dash';
+import PrimaryDash from './components/primary-dash';
 import Heading from './components/heading';
 import TransactionBar from './components/transaction-bar';
 import { Grid } from '@mui/material';
 
 function App() {
-
   return (
     <Grid container>
-      <Sidebar /> {/* Render Sidebar component*/}
-      <Grid item xs={9}>
+      {/* Sidebar Component */}
+      <Grid item xs={12} md={3}>
+        <Sidebar />
+      </Grid>
+      {/* Main Content Area */}
+      <Grid item xs={12} md={9}>
         <Grid container direction="column">
-          <Heading /> {/* Render Heading component */}
-          <PrimaryDash /> {/* Render PrimaryDash component*/}
+          {/* Heading Component */}
+          <Grid item>
+            <Heading />
+          </Grid>
+          {/* PrimaryDash Component */}
+          <Grid item>
+            <PrimaryDash />
+          </Grid>
         </Grid>
       </Grid>
-      <TransactionBar /> {/* Render TransactionBar component */}
+      {/* TransactionBar Component */}
+      <Grid item xs={12}>
+        <TransactionBar />
+      </Grid>
     </Grid>
   );
 }
 
-export default App
+export default App;
--- src/components/primary-dash.jsx
+++ src/components/primary-dash.jsx
@@ -1,12 +1,53 @@
-import { Grid } from "@mui/material";
+import React from 'react';
+import { Grid, Typography, Card, Paper } from '@mui/material';
 
-
-function PrimaryDash() {
-  return ( 
-    <Grid item>
-      
+const PrimaryDash = () => {
+  return (
+    <Grid container spacing={3}>
+      {/* Title */}
+      <Grid item xs={12}>
+        <Typography variant="h4" gutterBottom>
+          Dashboard Title
+        </Typography>
+      </Grid>
+      {/* Section 1 */}
+      <Grid item xs={12} sm={6}>
+        <Paper elevation={3} sx={{ height: '100%', p: 2 }}>
+          {/* Placeholder content for Section 1 */}
+          <Typography variant="h6" gutterBottom>
+            Section 1
+          </Typography>
+          <Typography variant="body1">
+            This is where your first section content goes.
+          </Typography>
+        </Paper>
+      </Grid>
+      {/* Section 2 */}
+      <Grid item xs={12} sm={6}>
+        <Paper elevation={3} sx={{ height: '100%', p: 2 }}>
+          {/* Placeholder content for Section 2 */}
+          <Typography variant="h6" gutterBottom>
+            Section 2
+          </Typography>
+          <Typography variant="body1">
+            This is where your second section content goes.
+          </Typography>
+        </Paper>
+      </Grid>
+      {/* Card */}
+      <Grid item xs={12}>
+        <Card sx={{ width: '100%' }}>
+          {/* Placeholder content for the card */}
+          <Typography variant="h6" gutterBottom>
+            Card Title
+          </Typography>
+          <Typography variant="body1">
+            This is a card content. You can add more cards or other components here.
+          </Typography>
+        </Card>
+      </Grid>
     </Grid>
-   );
-}
+  );
+};
 
 export default PrimaryDash;
